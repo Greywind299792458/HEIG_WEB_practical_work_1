24da6659ae802ccd2a2f61c6064ef1e1d8f963e7
Greywind299792458 <71220948+Greywind299792458@users.noreply.github.com>
2023-09-29 00:23:42 +0200

    🥅 managed API errors

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,22 @@
 const imageBtn = document.getElementById("generate-image-btn");
-console.log(imageBtn);
-
 if(imageBtn !== null) {
     imageBtn.addEventListener('click', () => {
         fetchNewImage();
-    })
+    });
 }
 
 function fetchNewImage() {
+    imageBtn.disabled = true;
     const imagePlaceholder = document.getElementById("placeholder-image");
     if(imagePlaceholder === null) { return; }
     fetch("https://cataas.com/cat")
     .then((response) => response.blob())
     .then((blob) => {
         imagePlaceholder.src = URL.createObjectURL(blob);
+        imageBtn.disabled = false;
+    })
+    .catch(() => {
+        imagePlaceholder.src = "assets/placeholder_cat.jpg";
+        imageBtn.disabled = false;
     });
 }
