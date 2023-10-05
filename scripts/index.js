ca6ce4b232a71f9af23168abef003b8054429a15
Greywind299792458 <71220948+Greywind299792458@users.noreply.github.com>
2023-10-05 17:59:47 +0200

    ✨ let the music play !

--- scripts/index.js
+++ scripts/index.js
@@ -9,10 +9,11 @@ function fetchNewImage() {
     imageBtn.disabled = true;
     const imagePlaceholder = document.getElementById("placeholder-image");
     if(imagePlaceholder === null) { return; }
-    fetch("https://cataas.com/cat")
-    .then((response) => response.blob())
-    .then((blob) => {
-        imagePlaceholder.src = URL.createObjectURL(blob);
+    // API documentation: https://developers.thecatapi.com/view-account/ylX4blBYT9FaoVd6OhvR?report=bOoHBz-8t
+    fetch("https://api.thecatapi.com/v1/images/search")
+    .then(response => response.json())
+    .then((data) => {
+        imagePlaceholder.src = data.length > 0 ? data[0].url : "assets/placeholder_cat.jpg"
         imageBtn.disabled = false;
     })
     .catch(() => {
@@ -20,3 +21,13 @@ function fetchNewImage() {
         imageBtn.disabled = false;
     });
 }
+
+let aud     = document.getElementById("audio");
+let fish    = document.getElementById("fish-img");
+
+aud.onpause = function() {
+    fish.classList.remove('rotating');
+};
+aud.onplay = function() {
+    fish.classList.add('rotating');
+};
