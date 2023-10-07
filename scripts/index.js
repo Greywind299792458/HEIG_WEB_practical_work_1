b4bef166890edb7b6a8a7411ea4fb6c8424abaeb
Greywind299792458 <71220948+Greywind299792458@users.noreply.github.com>
2023-10-07 16:03:25 +0200

    📝 documented JS code

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,45 @@
-const imageBtn = document.getElementById("generate-image-btn");
+// listen to the button click to fetch a new cat image
+const imageBtn = document.getElementById("fetch-image");
 if(imageBtn !== null) {
     imageBtn.addEventListener('click', () => {
         fetchNewImage();
     });
 }
 
+// fetch new cat image from the cat API and replaces the image
 function fetchNewImage() {
     imageBtn.disabled = true;
-    const imagePlaceholder = document.getElementById("placeholder-image");
+    const imagePlaceholder = document.getElementById("result-image");
     if(imagePlaceholder === null) { return; }
-    // API documentation: https://developers.thecatapi.com/view-account/ylX4blBYT9FaoVd6OhvR?report=bOoHBz-8t
+
     fetch("https://api.thecatapi.com/v1/images/search")
     .then(response => response.json())
     .then((data) => {
         imagePlaceholder.src = data.length > 0 ? data[0].url : "assets/placeholder_cat.jpg"
-        imageBtn.disabled = false;
     })
     .catch(() => {
         imagePlaceholder.src = "assets/placeholder_cat.jpg";
         imageBtn.disabled = false;
     });
+
+    imagePlaceholder.onload = function() {
+        imageBtn.disabled = false;
+    }
 }
 
-let aud     = document.getElementById("audio");
-let fish    = document.getElementById("fish-img");
+const aud   = document.getElementById("fish-audio");
+const fish  = document.getElementById("fish-img");
 
+// stop the fish rotation when the music stops
 aud.onpause = function() {
-    fish.classList.remove('rotating');
+    if(fish != null) {
+        fish.classList.remove('rotating');
+    }
 };
+
+// start the fish rotation when the music stops
 aud.onplay = function() {
-    fish.classList.add('rotating');
+    if(fish != null) {
+        fish.classList.add('rotating');
+    }
 };
